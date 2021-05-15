0efd9d4c8cb104070b5a8eab7db41a23a8ba1aa2
RomainRouxE <romain.roux@epitech.eu>
2021-05-15 16:53:35 +0200

    [FIX] light/dark mode preference

--- public/js/mode.js
+++ public/js/mode.js
@@ -1,10 +1,25 @@
+const page = window.location.href;
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+	document.body.classList.add("dark");
+	if (page.includes("home.html") || page.includes("home-en.html"))
+		document.getElementById("a").style.backgroundImage = "url('public/images/house/night.jpg')";
+}
+
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
-//	console.log(document.body.classList)
-	if(document.body.classList.contains("dark"))
-		document.getElementById("a").style.backgroundImage = "url('public/images/house/night.jpg')";
-	else
-	document.getElementById("a").style.backgroundImage = "url('public/images/house/day.jpg')";
+
+	let theme = "light";
+	if (document.body.classList.contains("dark")) {
+		theme = "dark";
+		if (page.includes("home.html") || page.includes("home-en.html"))
+			document.getElementById("a").style.backgroundImage = "url('public/images/house/night.jpg')";
+	}
+	else if (page.includes("home.html") || page.includes("home-en.html"))
+		document.getElementById("a").style.backgroundImage = "url('public/images/house/day.jpg')";
+
+	localStorage.setItem("theme", theme);
 });

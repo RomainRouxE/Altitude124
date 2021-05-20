7692cb8cb698555d9bf22f577de347063a1c88ec
RomainRouxE <romain.roux@epitech.eu>
2021-05-20 22:05:33 +0200

    [FIX] Change navbar on small device + fix some media queries

--- public/js/sticky.js
+++ public/js/sticky.js
@@ -1,13 +1,23 @@
-function stickyMenu() {
-    var sticky = document.getElementById("sticky");
-    if (window.pageYOffset > 20) {
-        sticky.classList.add("sticky");
-        sticky.classList.remove("menu-ul");
-    }
-    else {
-        sticky.classList.remove("sticky");
-    }
-}
-window.onscroll = function () {
-    stickyMenu();
-}
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+});

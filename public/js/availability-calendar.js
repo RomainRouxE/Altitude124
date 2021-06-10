2b5b1378ee781581d4d280b3c386ccddf57d038a
RomainRouxE <romain.roux@epitech.eu>
2021-06-10 15:16:48 +0200

    [ADD] format prettier + ajout nouvelle images maison

--- public/js/availability-calendar.js
+++ public/js/availability-calendar.js
@@ -1,210 +1,225 @@
 (function ($) {
-	var weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
+  function AvailabilityCalendar(container, bookedDates) {
+    this.date = new Date();
+    this.date.setDate(1);
 
-	function AvailabilityCalendar(container, bookedDates) {
-		this.date = new Date();
-		this.date.setDate(1);
+    this.container = container;
+    this.bookedDates = bookedDates;
 
-		this.container = container;
-		this.bookedDates = bookedDates;
+    this.createCalendar();
+    this.renderMonth();
+  }
 
-		this.createCalendar();
-		this.renderMonth();
-	}
+  AvailabilityCalendar.prototype = {
+    /**
+     * Setup methods
+     */
+    __createToolbar: function () {
+      var $toolbar = $("<div></div>").appendTo(this.container);
+      $toolbar.addClass("availability-calendar-toolbar");
 
+      this.$monthLabel = $("<span></span>").appendTo($toolbar);
+      var $inputContainer = $("<span></span>").appendTo($toolbar);
 
-	AvailabilityCalendar.prototype = {
-		/**
-		 * Setup methods
-		 */
-		__createToolbar: function () {
-			var $toolbar = $('<div></div>').appendTo(this.container);
-			$toolbar.addClass('availability-calendar-toolbar');
+      $inputContainer.append(
+        '<input type="button" title="Previous month" value="&#10094;">'
+      );
+      $inputContainer.append(
+        '<input type="button" title="Next month" value="&#10095;">'
+      );
 
-			this.$monthLabel = $('<span></span>').appendTo($toolbar);
-			var $inputContainer = $('<span></span>').appendTo($toolbar);
+      var $inputs = $inputContainer.children("input");
+      var self = this;
 
-			$inputContainer.append('<input type="button" title="Previous month" value="&#10094;">');
-			$inputContainer.append('<input type="button" title="Next month" value="&#10095;">');
+      $inputs.eq(0).on("click", function () {
+        self.date.setMonth(self.date.getMonth() - 1);
+        self.renderMonth();
+      });
 
-			var $inputs = $inputContainer.children('input');
-			var self = this;
+      $inputs.eq(1).on("click", function () {
+        self.date.setMonth(self.date.getMonth() + 1);
+        self.renderMonth();
+      });
+    },
+    __createTable: function () {
+      var $table = $("<table></table>").appendTo(this.container);
+      $table.addClass("availability-calendar");
 
+      // Weekday headers
+      var $tr = $("<tr></tr>").appendTo($table);
 
-			$inputs.eq(0).on('click', function () {
-				self.date.setMonth(self.date.getMonth() - 1);
-				self.renderMonth();
-			});
+      weekdays.forEach(function (day) {
+        $("<th></th>").html(day).appendTo($tr);
+      });
 
-			$inputs.eq(1).on('click', function () {
-				self.date.setMonth(self.date.getMonth() + 1);
-				self.renderMonth();
-			});
-		},
-		__createTable: function () {
-			var $table = $('<table></table>').appendTo(this.container);
-			$table.addClass('availability-calendar');
+      // Day cells
+      for (var i = 0; i < 6; ++i) {
+        $tr = $("<tr></tr>").appendTo($table);
+        $tr.append(
+          "<td></td><td></td><td></td><td></td><td></td><td></td><td></td>"
+        );
+      }
 
-			// Weekday headers
-			var $tr = $('<tr></tr>').appendTo($table);
+      this.$cells = $table.find("td");
+    },
+    createCalendar: function () {
+      this.__createToolbar();
+      this.__createTable();
+    },
 
-			weekdays.forEach(function (day) {
-				$('<th></th>').html(day).appendTo($tr);
-			});
+    /**
+     * Month rendering methods
+     */
+    __addPreviousMonthDays: function (date, cellIndexes, dates) {
+      var firstWeekdayOfMonth = date.getDay() - 1;
+      if (firstWeekdayOfMonth < 0) firstWeekdayOfMonth = 6;
 
-			// Day cells
-			for (var i = 0; i < 6; ++i) {
-				$tr = $('<tr></tr>').appendTo($table);
-				$tr.append('<td></td><td></td><td></td><td></td><td></td><td></td><td></td>');
-			}
+      if (firstWeekdayOfMonth > 0) {
+        date.setDate(0);
+        var numDays = date.getDate();
 
-			this.$cells = $table.find('td');
-		},
-		createCalendar: function () {
-			this.__createToolbar();
-			this.__createTable();
-		},
+        for (var i = numDays - firstWeekdayOfMonth + 1; i <= numDays; ++i) {
+          this.$cells.eq(dates.length).html(i).addClass("ex-month");
 
+          date.setDate(i);
+          var dateInt = date.valueOf();
 
-		/**
-		 * Month rendering methods
-		 */
-		__addPreviousMonthDays: function (date, cellIndexes, dates) {
-			var firstWeekdayOfMonth = date.getDay() - 1;
-			if (firstWeekdayOfMonth < 0) firstWeekdayOfMonth = 6;
+          cellIndexes[dateInt] = dates.length;
+          dates.push(dateInt);
+        }
+      }
+    },
+    __addThisMonthDays: function (date, year, month, cellIndexes, dates) {
+      date.setFullYear(year, month + 1, 0); // Need to reset year
+      var numDays = date.getDate();
 
-			if (firstWeekdayOfMonth > 0) {
-				date.setDate(0);
-				var numDays = date.getDate();
+      for (var i = 1; i <= numDays; ++i) {
+        this.$cells.eq(dates.length).html(i);
 
-				for (var i = numDays - firstWeekdayOfMonth + 1; i <= numDays; ++i) {
-					this.$cells.eq(dates.length).html(i).addClass('ex-month');
+        date.setDate(i);
+        var dateInt = date.valueOf();
 
-					date.setDate(i);
-					var dateInt = date.valueOf();
+        cellIndexes[dateInt] = dates.length;
+        dates.push(dateInt);
+      }
+    },
+    __addNextMonthDays: function (date, month, cellIndexes, dates) {
+      if (dates.length < 42) {
+        date.setMonth(month + 1, 1);
+        var remainingDays = 42 - dates.length;
 
-					cellIndexes[dateInt] = dates.length;
-					dates.push(dateInt);
-				}
-			}
-		},
-		__addThisMonthDays: function (date, year, month, cellIndexes, dates) {
-			date.setFullYear(year, month + 1, 0); // Need to reset year
-			var numDays = date.getDate();
+        for (var i = 1; i <= remainingDays; ++i) {
+          this.$cells.eq(dates.length).html(i).addClass("ex-month");
 
-			for (var i = 1; i <= numDays; ++i) {
-				this.$cells.eq(dates.length).html(i);
+          date.setDate(i);
+          var dateInt = date.valueOf();
 
-				date.setDate(i);
-				var dateInt = date.valueOf();
+          cellIndexes[dateInt] = dates.length;
+          dates.push(dateInt);
+        }
+      }
+    },
+    __addEvents: function (cellIndexes, dates) {
+      var firstDate = dates[0];
+      var lastDate = dates[dates.length - 1];
+      var self = this;
 
-				cellIndexes[dateInt] = dates.length;
-				dates.push(dateInt);
-			}
-		},
-		__addNextMonthDays: function (date, month, cellIndexes, dates) {
-			if (dates.length < 42) {
-				date.setMonth(month + 1, 1);
-				var remainingDays = 42 - dates.length;
+      this.bookedDates.forEach(function (date) {
+        if (date.start <= lastDate && date.end >= firstDate) {
+          var startIndex = cellIndexes[date.start];
+          var endIndex = cellIndexes[date.end];
 
-				for (var i = 1; i <= remainingDays; ++i) {
-					this.$cells.eq(dates.length).html(i).addClass('ex-month');
+          if (startIndex !== undefined) {
+            self.$cells.eq(startIndex).addClass("unavailable");
+            ++startIndex;
+          } else {
+            startIndex = cellIndexes[firstDate];
+          }
 
-					date.setDate(i);
-					var dateInt = date.valueOf();
+          if (endIndex !== undefined) {
+            self.$cells.eq(endIndex).addClass("unavailable");
+            --endIndex;
+          } else {
+            endIndex = cellIndexes[lastDate];
+          }
 
-					cellIndexes[dateInt] = dates.length;
-					dates.push(dateInt);
-				}
-			}
-		},
-		__addEvents: function (cellIndexes, dates) {
-			var firstDate = dates[0];
-			var lastDate = dates[dates.length - 1];
-			var self = this;
+          self.$cells.slice(startIndex, endIndex + 1).addClass("unavailable");
+        }
+      });
+    },
+    renderMonth: function () {
+      var cellIndexes = {};
+      var dates = [];
 
-			this.bookedDates.forEach(function (date) {
-				if (date.start <= lastDate && date.end >= firstDate) {
-					var startIndex = cellIndexes[date.start];
-					var endIndex = cellIndexes[date.end];
+      var year = this.date.getFullYear();
+      var month = this.date.getMonth();
+      var date = new Date(year, month, 1);
 
-					if (startIndex !== undefined) {
-						self.$cells.eq(startIndex).addClass('unavailable');
-						++startIndex;
-					}
-					else {
-						startIndex = cellIndexes[firstDate];
-					}
+      this.$monthLabel.html(months[month] + " " + year);
+      this.$cells.removeClass("ex-month");
+      this.$cells
+        .filter(".unavailable")
+        .removeClass("unavailable")
+        .children()
+        .remove();
 
-					if (endIndex !== undefined) {
-						self.$cells.eq(endIndex).addClass('unavailable');
-						--endIndex;
-					}
-					else {
-						endIndex = cellIndexes[lastDate];
-					}
+      this.__addPreviousMonthDays(date, cellIndexes, dates);
+      this.__addThisMonthDays(date, year, month, cellIndexes, dates);
+      this.__addNextMonthDays(date, month, cellIndexes, dates);
 
-					self.$cells.slice(startIndex, endIndex + 1).addClass('unavailable');
-				}
-			});
-		},
-		renderMonth: function () {
-			var cellIndexes = {};
-			var dates = [];
+      this.__addEvents(cellIndexes, dates);
+    },
+  };
 
-			var year = this.date.getFullYear();
-			var month = this.date.getMonth();
-			var date = new Date(year, month, 1);
+  $.fn.availabilityCalendar = function (bookedDates) {
+    var dates = [];
 
-			this.$monthLabel.html(months[month] + ' ' + year);
-			this.$cells.removeClass('ex-month');
-			this.$cells.filter('.unavailable').removeClass('unavailable').children().remove();
+    bookedDates.forEach(function (date) {
+      var start = new Date(date.start);
+      var end = new Date(date.end);
 
-			this.__addPreviousMonthDays(date, cellIndexes, dates);
-			this.__addThisMonthDays(date, year, month, cellIndexes, dates);
-			this.__addNextMonthDays(date, month, cellIndexes, dates);
+      start.setHours(0, 0, 0, 0);
+      end.setHours(0, 0, 0, 0);
 
-			this.__addEvents(cellIndexes, dates);
-		}
-	};
+      start = start.valueOf();
+      end = end.valueOf();
 
+      if (start <= end) {
+        dates.push({
+          start: start,
+          end: end,
+        });
+      }
+    });
 
-	$.fn.availabilityCalendar = function (bookedDates) {
-		var dates = [];
+    this.each(function () {
+      new AvailabilityCalendar(this, dates);
+    });
 
-		bookedDates.forEach(function (date) {
-			var start = new Date(date.start);
-			var end = new Date(date.end);
-
-			start.setHours(0, 0, 0, 0);
-			end.setHours(0, 0, 0, 0);
-
-			start = start.valueOf();
-			end = end.valueOf();
-
-			if (start <= end) {
-				dates.push({
-					start: start,
-					end: end
-				});
-			}
-		});
-
-		this.each(function () {
-			new AvailabilityCalendar(this, dates);
-		});
-
-		return this;
-	};
+    return this;
+  };
 })(jQuery);
 
-
 var unavailableDates = [
-	{ start: '2021-09-11', end: '2021-09-15' },
-	{ start: '2022-09-15', end: '2022-09-23' },
-	{ start: '2021-10-01', end: '2021-10-07' }
+  { start: "2021-09-11", end: "2021-09-15" },
+  { start: "2022-09-15", end: "2022-09-23" },
+  { start: "2021-10-01", end: "2021-10-07" },
 ];
 
-$('#calendar').availabilityCalendar(unavailableDates);
+$("#calendar").availabilityCalendar(unavailableDates);
--- public/js/mode.js
+++ public/js/mode.js
@@ -2,24 +2,26 @@ const page = window.location.href;
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme == "dark") {
-	document.body.classList.add("dark");
-	if (page.includes("home.html") || page.includes("home-en.html"))
-		document.getElementById("a").style.backgroundImage = "url('public/images/house/night.jpg')";
+  document.body.classList.add("dark");
+  if (page.includes("home.html") || page.includes("home-en.html"))
+    document.getElementById("a").style.backgroundImage =
+      "url('public/images/house/bgdark.jpg')";
 }
 
-const chk = document.getElementById('chk');
+const chk = document.getElementById("chk");
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
 
-	let theme = "light";
-	if (document.body.classList.contains("dark")) {
-		theme = "dark";
-		if (page.includes("home.html") || page.includes("home-en.html"))
-			document.getElementById("a").style.backgroundImage = "url('public/images/house/night.jpg')";
-	}
-	else if (page.includes("home.html") || page.includes("home-en.html"))
-		document.getElementById("a").style.backgroundImage = "url('public/images/house/day.jpg')";
+  let theme = "light";
+  if (document.body.classList.contains("dark")) {
+    theme = "dark";
+    if (page.includes("home.html") || page.includes("home-en.html"))
+      document.getElementById("a").style.backgroundImage =
+        "url('public/images/house/bgdark.jpg')";
+  } else if (page.includes("home.html") || page.includes("home-en.html"))
+    document.getElementById("a").style.backgroundImage =
+      "url('public/images/house/bglight.jpg')";
 
-	localStorage.setItem("theme", theme);
+  localStorage.setItem("theme", theme);
 });
